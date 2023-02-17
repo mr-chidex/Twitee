ff88bf1068dee7fa7874589c60c22050d6a77121
mr-chidex <mbahchiboy2@gmail.com>
2023-02-17 03:47:00 +0100

    update auth

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,3 +1,11 @@
-class AuthService {}
+class AuthService {
+  async register(body: any) {
+    return {};
+  }
+
+  async login(body: any) {
+    return {};
+  }
+}
 
 export const authService = new AuthService();
