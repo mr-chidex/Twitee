8aa519f3b8b150da3656163516abffa6ec4bdc11
mr-chidex <mbahchiboy2@gmail.com>
2023-02-23 18:50:32 +0100

    auth test done

--- src/tests/unit test/services/auth.service.spec.ts
+++ src/tests/unit test/services/auth.service.spec.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt';
 import JWT from 'jsonwebtoken';
-import { Prisma } from '../../../prisma';
 
+import { IUser, Prisma } from '../../../prisma';
 import { authService } from '../../../services';
 import { mockUser } from '../../mocks';
 
@@ -76,6 +76,100 @@ describe('AuthService', () => {
 
       expect(bcrypt.hash).toHaveBeenCalledWith(mockUser.password, '1234');
       expect(response).toEqual('hashedPass');
+    });
+  });
+
+  describe('register', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
+    it('should create a user', async () => {
+      authService.validateRegisterationParams = jest.fn().mockReturnValue(null);
+      authService.validateRegisterationEmail = jest.fn().mockResolvedValue(null);
+      authService.hashPassword = jest.fn().mockResolvedValue('hashed');
+      Prisma.user.create = jest.fn().mockResolvedValue(true);
+
+      const response = await authService.register(mockUser);
+
+      expect(response.success).toBe(true);
+      expect(response.message).toBe('Account successfully created');
+      expect(Prisma.user.create).toHaveBeenCalled();
+    });
+  });
+
+  describe('validateCredentials', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
+    it('should throw error on incorrect email', () => {
+      Prisma.user.findUnique = jest.fn().mockResolvedValue(null);
+
+      expect(authService.validateCredentials.bind('incorrect', 'correct')).rejects.toThrow(
+        'Email or Password is incorrect',
+      );
+      expect(Prisma.user.findUnique).toHaveBeenCalled();
+    });
+
+    it('should throw error on incorrect password', () => {
+      Prisma.user.findUnique = jest.fn().mockResolvedValue(mockUser);
+      bcrypt.compare = jest.fn().mockResolvedValue(false);
+
+      expect(authService.validateCredentials.bind(this, 'test@email.com', '')).rejects.toThrow(
+        'Email or Password is incorrect',
+      );
+    });
+
+    it('should return user', async () => {
+      Prisma.user.findUnique = jest.fn().mockResolvedValue(mockUser);
+      bcrypt.compare = jest.fn().mockResolvedValue(true);
+
+      const response = await authService.validateCredentials('test@email.com', 'test123');
+
+      expect(response).toEqual(mockUser);
+    });
+  });
+
+  describe('getToken', () => {
+    it('should return token', () => {
+      jest.spyOn(JWT, 'sign').mockReturnValue();
+
+      expect(JWT.sign).not.toHaveBeenCalled();
+
+      authService.getToken(mockUser);
+
+      expect(JWT.sign).toHaveBeenCalled();
+    });
+  });
+
+  describe('login', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      jest.resetAllMocks();
+    });
+
+    it('should throw error on incorrect details', () => {
+      const params = { email: 'incorrect', passsword: 'test' };
+
+      expect(authService.login.bind(this, params)).rejects.toThrow();
+    });
+
+    it('should return token on successful login', async () => {
+      const user = { email: 'test@email.com', password: 'test123' } as IUser;
+
+      authService.validateCredentials = jest.fn().mockResolvedValue(user);
+      authService.getToken = jest.fn().mockReturnValue('token');
+
+      expect(authService.getToken).not.toHaveBeenCalled();
+
+      const response = await authService.login(user);
+
+      expect(authService.getToken).toHaveBeenCalledWith(user);
+      expect(response.success).toBe(true);
+      expect(response.data).toBe('token');
     });
   });
 });
